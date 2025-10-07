c350103d77c66bcc7c39c5a96ae2cd1ac83038fb
K4LCHAKRA <oseverest44@gmail.com>
2025-10-07 16:45:48 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -6,8 +6,8 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/mojito
 rm -rf vendor/xiaomi/sm6150-common
 
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
-/opt/crave/resync.sh
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 rm -rf device/xiaomi/mojito
 rm -rf device/xiaomi/sm6150-common
@@ -16,20 +16,13 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/mojito
 rm -rf vendor/xiaomi/sm6150-common
 
-# device/xiaomi/mojito
-git clone https://github.com/BlissRoms-Devices/device_xiaomi_mojito -b wip device/xiaomi/mojito
+git clone https://github.com/Infinity-X-Devices/android_device_xiaomi_mojito.git -b 16 device/xiaomi/mojito
+git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 16 device/xiaomi/sm6150-common
+git clone https://github.com/Infinity-X-Devices/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://gitlab.com/romgharti/android_vendor_xiaomi_mojito.git --depth 1 -b 16 vendor/xiaomi/mojito
+git clone https://gitlab.com/romgharti/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 16 vendor/xiaomi/sm6150-common
+git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
-# device/xiaomi/sm6150-common
-git clone https://github.com/BlissRoms-Devices/android_device_xiaomi_sm6150-common -b wip device/xiaomi/sm6150-common
-
-# kernel/xiaomi/mojito
-git clone https://github.com/BlissRoms-Devices/kernel_xiaomi_mojito --depth 1 -b inline-rom kernel/xiaomi/mojito
-
-# hardware/xiaomi
-git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi --depth 1 -b mojito hardware/xiaomi
-
-# vendor/xiaomi/mojito
-git clone https://gitlab.com/romgharti/android_vendor_xiaomi_mojito --depth 1 -b 15 vendor/xiaomi/mojito
-
-# vendor/xiaomi/sm6150-common
-git clone https://gitlab.com/romgharti/android_vendor_xiaomi_sm6150-common --depth 1 -b 15 vendor/xiaomi/sm6150-common
+. build/envsetup.sh
+lunch infinity-mojito-user
+bacon
