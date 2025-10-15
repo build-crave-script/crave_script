8a954c1230b2d840f50a77b4c3199855010221dd
K4LCHAKRA <oseverest44@gmail.com>
2025-10-15 23:13:18 +0545

    Uncomment repo commands in crave.sh

--- crave.sh
+++ crave.sh
@@ -1,7 +1,7 @@
 
-# repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 # repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-# /opt/crave/resync.sh
+/opt/crave/resync.sh
 . build/envsetup.sh
 lunch infinity-mojito-user
 bacon
