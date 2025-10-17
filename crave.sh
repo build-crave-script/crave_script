beb5a7c9000e3058624c80b786cd514f50b70e6d
K4LCHAKRA <oseverest44@gmail.com>
2025-10-17 12:09:42 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,8 +1,8 @@
-# repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
-# /opt/crave/resync.sh
-
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 /opt/crave/resync.sh
+
+# repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+# /opt/crave/resync.sh
 
 . build/envsetup.sh
 lunch infinity-mojito-user
