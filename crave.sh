cbe88928bb33a0d144bb61929d09ec1a0d7c40c3
K4LCHAKRA <oseverest44@gmail.com>
2025-10-27 11:24:38 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,9 +1,9 @@
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
-/opt/crave/resync.sh
-
-# repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+# repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 # /opt/crave/resync.sh
 
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+/opt/crave/resync.sh
+git clone https://github.com/Infinity-X-Devices/android_device_xiaomi_mojito.git -b 16 device/xiaomi/mojito
 . build/envsetup.sh
-lunch lineage-mojito-user
+lunch infinity-mojito-user
 bacon
