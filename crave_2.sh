607a9863bbf589b1603ece81ae6e3ac0a809a343
K4LCHAKRA <oseverest44@gmail.com>
2025-11-14 12:50:30 +0545

    Update crave_2.sh

--- crave_2.sh
+++ crave_2.sh
@@ -1,6 +1,7 @@
 repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 /opt/crave/resync.sh
-git clone https://github.com/RisingOS-Revived-devices/android_device_xiaomi_onyx.git -b sixteen device/xiaomi/onyx
+rm -rf device/xiaomi/onyx
+# git clone https://github.com/RisingOS-Revived-devices/android_device_xiaomi_onyx.git -b sixteen device/xiaomi/onyx
 . build/envsetup.sh
 riseup onyẋ user
 rise ḅ
